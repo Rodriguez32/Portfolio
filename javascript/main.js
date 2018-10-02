b2824c82f96933a5016b083f8cf4be6e66c183c1
Azalea <Rodriguez32.ilda0122@gmail.com>
2018-10-01 20:34:12 -0500

    added changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,17 @@
 
-
+// Side nav //
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+$(".button-collapse").sidenav();
+
+  // parallax //
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
        
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
